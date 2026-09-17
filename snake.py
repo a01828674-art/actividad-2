43d6b4ba8bbbbd5247d83a39bab399221b8bf4fd
Alex <a01828674@tec.mx>
2026-09-17 07:34:30 -0600

    Se agrega movimiento al azar de la fruta

--- snake.py
+++ snake.py
@@ -7,10 +7,8 @@ Exercises
 3. How would you move the food?
 4. Change the snake to respond to mouse clicks.
 """
-
-from random import randrange
+from random import choice, randrange
 from turtle import *
-
 from freegames import square, vector
 
 food = vector(0, 0)
@@ -19,43 +17,52 @@ aim = vector(0, -10)
 
 
 def change(x, y):
-    """Change snake direction."""
-    aim.x = x
-    aim.y = y
+  """Change snake direction."""
+  aim.x = x
+  aim.y = y
 
 
 def inside(head):
-    """Return True if head inside boundaries."""
-    return -200 < head.x < 190 and -200 < head.y < 190
+  """Return True if head inside boundaries."""
+  return -200 < head.x < 190 and -200 < head.y < 190
 
 
 def move():
-    """Move snake forward one segment."""
-    head = snake[-1].copy()
-    head.move(aim)
+  """Move snake forward one segment."""
+  head = snake[-1].copy()
+  head.move(aim)
 
-    if not inside(head) or head in snake:
-        square(head.x, head.y, 9, 'red')
-        update()
-        return
-
-    snake.append(head)
-
-    if head == food:
-        print('Snake:', len(snake))
-        food.x = randrange(-15, 15) * 10
-        food.y = randrange(-15, 15) * 10
-    else:
-        snake.pop(0)
-
-    clear()
-
-    for body in snake:
-        square(body.x, body.y, 9, 'black')
-
-    square(food.x, food.y, 9, 'green')
+  if not inside(head) or head in snake:
+    square(head.x, head.y, 9, 'red')
     update()
-    ontimer(move, 100)
+    return
+
+  snake.append(head)
+
+  if head == food:
+    print('Snake:', len(snake))
+    food.x = randrange(-15, 15) * 10
+    food.y = randrange(-15, 15) * 10
+  else:
+    snake.pop(0)
+
+  # Mover la comida al azar un paso a la vez sin salir de la ventana
+  food_aim = choice(
+      [vector(10, 0), vector(-10, 0), vector(0, 10), vector(0, -10)]
+  )
+  new_food = food.copy()
+  new_food.move(food_aim)
+  if inside(new_food):
+    food.move(food_aim)
+
+  clear()
+
+  for body in snake:
+    square(body.x, body.y, 9, 'black')
+
+  square(food.x, food.y, 9, 'green')
+  update()
+  ontimer(move, 200)
 
 
 setup(420, 420, 370, 0)
